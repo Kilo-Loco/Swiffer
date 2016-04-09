f81f1a4b3cab452130654887491727f2820c0b23
Kyle Lee <kiloloco@me.com>
2016-04-08 23:56:45 -0700

    Working on making sweets show in dashboard

---
+++ Swiffer/SweetsTableViewController.swift
@@ -0,0 +1,128 @@
+//
+//  SweetsTableViewController.swift
+//  Swiffer
+//
+//  Created by Kyle Lee on 4/8/16.
+//  Copyright © 2016 Kilo Loco. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+class SweetsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    @IBAction func sendSweet(sender: AnyObject) {
+        
+        let alert = UIAlertController(title: "New Sweet", message: "Enter a Sweet", preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (textField: UITextField) in
+            textField.placeholder = "Your Sweet"
+        }
+        print("here")
+        alert.addAction(UIAlertAction(title: "Send", style: .Default, handler: { (action: UIAlertAction) in
+            let textField = alert.textFields!.first!
+            
+            if textField.text != "" {
+                let newSweet = CKRecord(recordType: "Sweet")
+                newSweet["content"] = textField.text
+                print("then here")
+                let publicData = CKContainer.defaultContainer().publicCloudDatabase
+                publicData.saveRecord(newSweet, completionHandler: { (record: CKRecord?, error: NSError?) in
+                    if error == nil {
+                        print("sweet saved")
+                    }
+                    print(error)
+                })
+                
+                
+            }
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
